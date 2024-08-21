8896c7061f7a55015f4d2432fdf802c48de98a8f
cyphersept <95666934+cyphersept@users.noreply.github.com>
2024-08-21 03:00:20 -0400

    Style daily weather panels

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { APIs, Utils } from "./Weather.js";
-import bg from "./assets/neon-bg-hd.webp";
-import bg4k from "./assets/neon-bg-4k.webp";
+import { SvgButton, SvgBoxTabbed } from "./SVGs.jsx";
 import "./index.css";
 import "./css/weather-icons.min.css";
 
@@ -91,35 +90,40 @@ function Daily({ data }) {
 }
 
 function Day({ data }) {
+  const weatherDescription = data.interpreted_code.description;
   return (
-    <div className="day uppercase bg-[url('./assets/box-tab.svg')] bg-no-repeat w-56 h-32 bg-[100%_auto]">
-      <div className="content ml-12 p-1">
-        <div className="time text-[1.25em] font-bold text-shadow-sm shadow-purple-1 text-magenta">
-          {data.time.slice(5).replace("-", "/")}
+    <div className="day uppercase w-56 h-36 bg-[100%_auto] text-[0.5rem] relative font-semibold">
+      <SvgBoxTabbed />
+      <div className="content pt-1.5 pl-11 text-base tracking-tight min-h-28">
+        <div className="-ml-6 font-bold text-shadow-sm shadow-white h-6">
+          <span className="time text-[1.05rem] font-extrabold text-[hotpink] align-top">
+            {data.time.slice(5).replace("-", "/")} -{" "}
+          </span>
+          {weatherDescription.length < 14 ? (
+            <span>{weatherDescription}</span>
+          ) : (
+            <Marquee content={weatherDescription} />
+          )}
         </div>
         <i
           className={
-            "wi text-shadow-sm shadow-white my-2 text-[1.5em] md:text-[2.5em] float-left " +
+            "wi text-shadow-sm shadow-white text-[1.5em] md:text-[2.5em] mx-2 mt-2 float-right " +
             data.interpreted_code.icon
           }
         ></i>
-        <Marquee content={data.interpreted_code.description} />
-        <Marquee
-          content={
-            <>
-              //:Temp: {data.apparent_temperature_min}
-              <span className="text-magenta"> / </span>
-              {data.apparent_temperature_max}
-              //:Feel: {data.apparent_temperature_min}
-              <span className="text-magenta"> / </span>
-              {data.apparent_temperature_max}
-            </>
-          }
-        />
-        <Marquee
-          className="precipitation"
-          content={"Precipitation: " + data.precipitation_probability_max}
-        />
+        <div className="temp mt-1">
+          {data.apparent_temperature_min}
+          <span className="text-magenta font-bold"> / </span>
+          {data.apparent_temperature_max}
+        </div>
+        <div className="feel">
+          {data.apparent_temperature_min}
+          <span className="text-magenta font-bold"> / </span>
+          {data.apparent_temperature_max}
+        </div>
+        <div className="precipitation">
+          Precipitation: {data.precipitation_probability_max}
+        </div>
       </div>
     </div>
   );
@@ -127,10 +131,9 @@ function Day({ data }) {
 
 function Marquee({ content }) {
   return (
-    <div className="wrapper overflow-hidden h-6">
-      <div className="text-scroll whitespace-nowrap">
-        <p className="inline-block">{content}</p>
-        <p className="inline-block">{content}</p>
+    <div className="wrapper inline-block overflow-hidden h-[1.375rem] ">
+      <div className="text-scroll inline-block min-w-full whitespace-nowrap">
+        {content}
       </div>
     </div>
   );
@@ -157,191 +160,10 @@ function Search({ onInputChange, onSearch, inputValue, geoData }) {
           placeholder="Lookup location..."
           value={inputValue}
           onChange={(event) => onInputChange(event.target.value)}
-          className="h-12 p-4 rounded-sm min-w-80 w-3/5 max-w-3/4 text-xl border-white border-double border-4 bg-purple-4 color1"
+          className="h-12 p-4 rounded-sm min-w-80 w-3/5 max-w-3/4 text-xl border-white border-double border-4 bg-purple-4 purplish"
         />
         <SvgButton onClick={onSearch} text="SEARCH"></SvgButton>
       </div>
-    </div>
-  );
-}
-
-function SvgButton({ onClick, text }) {
-  return (
-    <div className="absolute searchButton w-80 color1 glow -top-4 right-0 -translate-x-1/2">
-      <div className="absolute font-bold text-3xl left-[55%] text-shadow-sm shadow-white top-1/3 -translate-x-1/2 -translate-y-1/2 z-10 pointer-events-none">
-        {text}
-      </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="1076.97 251.63 951.42 327.08"
-        role="button"
-        onClick={onClick}
-      >
-        <g>
-          <polygon
-            className="group3"
-            points="1384.577,367.148 1336.55,415.174 1307.688,415.174 1344.421,378.441 1355.715,367.148     1344.421,355.855 1307.688,319.122 1336.55,319.122   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1352.563,319.119 1400.591,367.148 1352.563,415.176 1384.581,415.176 1432.609,367.148     1384.581,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1400.591,319.119 1448.619,367.148 1400.591,415.176 1432.609,415.176 1480.638,367.148     1432.609,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1448.619,319.119 1496.647,367.148 1448.619,415.176 1480.638,415.176 1528.666,367.148     1480.638,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1496.647,319.119 1544.676,367.148 1496.647,415.176 1528.666,415.176 1576.694,367.148     1528.666,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1544.676,319.119 1592.704,367.148 1544.676,415.176 1576.694,415.176 1624.723,367.148     1576.694,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1592.704,319.119 1640.732,367.148 1592.704,415.176 1624.723,415.176 1672.751,367.148     1624.723,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1640.732,319.119 1688.761,367.148 1640.732,415.176 1672.751,415.176 1720.779,367.148     1672.751,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1688.761,319.119 1736.789,367.148 1688.761,415.176 1720.779,415.176 1768.808,367.148     1720.779,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1736.789,319.119 1784.817,367.148 1736.789,415.176 1768.808,415.176 1816.836,367.148     1768.808,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1784.817,319.119 1832.846,367.148 1784.817,415.176 1816.836,415.176 1864.864,367.148     1816.836,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1832.846,319.119 1880.874,367.148 1832.846,415.176 1864.864,415.176 1912.893,367.148     1864.864,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1880.874,319.119 1928.902,367.148 1880.874,415.176 1912.893,415.176 1960.921,367.148     1912.893,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="1928.902,319.119 1976.931,367.148 1928.902,415.176 1960.921,415.176 2008.949,367.148     1960.921,319.119   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="2008.949,319.119 2008.949,319.119 1976.931,319.119 2008.949,351.138   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group3"
-            points="2008.949,383.157 1976.931,415.176 2008.949,415.176 2008.949,415.176   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group2"
-            points="2028.381,299.683 2028.381,434.613 1288.25,434.613 1295.095,427.769 2021.536,427.769     2021.536,306.528 1295.095,306.528 1288.25,299.683   "
-          />
-        </g>
-        <g>
-          <g>
-            <g>
-              <path
-                className="group2"
-                d="M1256.514,290.534l-76.614,76.614l76.614,76.614l15.994-15.994l60.621-60.621L1256.514,290.534z       M1256.514,418.597l-51.471-51.449l51.471-51.449l51.449,51.449L1256.514,418.597z"
-              />
-            </g>
-            <g>
-              <path
-                className="fill-white"
-                d="M1165.298,367.148l45.608-45.608l-3.993-3.993l-38.307,38.307l-11.293,11.293l11.293,11.294      l56.742,56.742l3.97-3.993L1165.298,367.148z"
-              />
-            </g>
-          </g>
-        </g>
-        <g>
-          <path
-            className="group1"
-            d="M1367.146,251.656l-38.854,38.855h265.822l16.017-15.994h96.053l22.861-22.861H1367.146z     M1703.353,267.673H1607.3l-16.017,15.994h-246.475l25.188-25.165h342.527L1703.353,267.673z"
-          />
-        </g>
-        <g>
-          <path
-            className="group2"
-            d="M1875.176,283.667h-258.978l-22.861,22.861h304.699L1875.176,283.667z"
-          />
-        </g>
-        <g>
-          <polygon
-            className="group1"
-            points="1488.888,443.762 1466.05,466.623 1385.991,466.623 1353.981,498.656 1216.222,498.656     1252.521,462.357 1271.093,443.762   "
-          />
-        </g>
-        <g>
-          <polygon
-            className="group2"
-            points="1370.687,498.656 1388.984,480.358 1480.474,480.358 1517.07,443.762 1809.837,443.762     1791.539,462.06 1517.07,462.06 1480.474,498.656   "
-          />
-        </g>
-        <g>
-          <path
-            className="group2"
-            d="M1229.318,431.191l-3.97,3.993l-99.27,99.247l-2.441,2.441c-1.369-1.848-2.988-3.468-4.836-4.837    l100.844-100.844l-64.042-64.043l50.467-50.445l4.837,4.837l-45.608,45.608L1229.318,431.191z"
-          />
-        </g>
-        <path
-          className="group2"
-          d="M1126.078,534.431c-1.415-1.803-3.034-3.422-4.837-4.837c-4.654-3.605-10.495-5.772-16.815-5.772   c-15.149,0-27.447,12.297-27.447,27.424c0,15.15,12.298,27.447,27.447,27.447c15.127,0,27.424-12.297,27.424-27.447   C1131.851,544.926,1129.683,539.085,1126.078,534.431z M1104.426,575.27c-13.278,0-24.024-10.746-24.024-24.025   c0-13.255,10.746-24.001,24.024-24.001c5.407,0,10.381,1.78,14.374,4.791c1.848,1.369,3.468,2.989,4.836,4.837   c3.012,3.993,4.792,8.966,4.792,14.373C1128.428,564.524,1117.682,575.27,1104.426,575.27z"
-        />
-        <path
-          className="group1"
-          d="M1104.426,541.115c-5.612,0-10.153,4.541-10.153,10.13c0,5.613,4.54,10.153,10.153,10.153   c5.59,0,10.13-4.541,10.13-10.153C1114.557,545.656,1110.016,541.115,1104.426,541.115z"
-        />
-        <g>
-          <g>
-            <path
-              className="group1"
-              d="M1256.514,338.264l-28.884,28.884l28.884,28.862l28.862-28.862L1256.514,338.264z"
-            />
-          </g>
-        </g>
-      </svg>
     </div>
   );
 }
